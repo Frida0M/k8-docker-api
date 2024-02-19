e85bac2072868311244841dd2e795035433e0e94
Frida Olivera Martínez <90354687+Frida0M@users.noreply.github.com>
2024-02-18 21:52:03 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql2');
+
+app.get('/', (req, res) => {
+    res.send('Servidor de la aplicación.');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
+
+
+const db = mysql.createConnection({
+    host: 'mysql-container',
+    user: 'root',
+    password: 'password',
+    database: 'mydb',
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error al conectar a MySQL:', err);
+    } else {
+        console.log('Conexión exitosa a MySQL');
+    }
+});
